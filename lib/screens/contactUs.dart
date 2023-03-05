57d9a7c308e04e9c12601e6617c25f486cc7c7fc
musharaf8651 <musharaf8651>
2023-03-05 23:17:00 +0530

    routes defined

---
+++ lib/screens/contactUs.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ContactUs extends StatefulWidget {
+  const ContactUs({super.key});
+
+  @override
+  State<ContactUs> createState() => _ContactUsState();
+}
+
+class _ContactUsState extends State<ContactUs> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
