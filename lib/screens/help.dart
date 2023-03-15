ae8851e2347c6ed3efae58e5c84ffb9889a39901
musharaf8651 <musharaf8651>
2023-03-15 18:20:47 +0530

    notification and calendar added

---
+++ lib/screens/help.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Help extends StatelessWidget {
+  const Help({super.key});
+  static const routeName = '/help';
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screens/homeScreen/calendar.dart
+++ lib/screens/homeScreen/calendar.dart
@@ -1,5 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:calendar_view/calendar_view.dart';
+import 'package:garbage_grinder/widgets/date_calendar.dart';
+import 'package:responsive_widgets/responsive_widgets.dart';
 
 class Calnedar extends StatefulWidget {
   const Calnedar({super.key});
@@ -11,6 +15,6 @@ class Calnedar extends StatefulWidget {
 class _CalnedarState extends State<Calnedar> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const DateCalendar();
   }
 }
