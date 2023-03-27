822600f7e6b7a6986105f99013aa5ec12b123494
Vikrant Singh <vikrantsingh1889@gmail.com>
2023-03-27 22:19:51 +0530

    Edited 'calendar.dart' and
    Added a simple calender like interface
    
    Still no ux

--- lib/screens/homeScreen/calendar.dart
+++ lib/screens/homeScreen/calendar.dart
@@ -1,33 +1,179 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:calendar_view/calendar_view.dart';
-import 'package:garbage_grinder/widgets/date_calendar.dart';
 
-class Calnedar extends StatefulWidget {
-  const Calnedar({super.key});
+import 'dart:math';
+
+const int cellCount = 100;
+const int rows = 5;
+const int columns = 17;
+const String year = '2023';
+
+class Calendar extends StatefulWidget {
+  const Calendar({super.key});
+
 
   @override
-  State<Calnedar> createState() => _CalnedarState();
+  State<Calendar> createState() => _CalendarState();
 }
 
-class _CalnedarState extends State<Calnedar> {
+class _CalendarState extends State<Calendar> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: [
-            Center(
-              child: Container(
-                height: 500,
-                width: 300,
-                child: DateCalendar(),
+    return Padding(
+        padding: EdgeInsets.symmetric(horizontal: 5),
+        child: Column(
+      children: [
+        const SizedBox(height: 20),
+            const Text(
+              year,
+              style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                  decoration: TextDecoration.underline,
+                  decorationStyle: TextDecorationStyle.solid
               ),
             ),
-          ],
+        const SizedBox(height: 20),
+        Table(
+          children: List.generate(rows, (row) {
+            List<TableCell> rowCells = [];
+            for (int column = 0; column < columns; column++) {
+              int cellidx = (row * columns + column);
+              if (cellidx < cellCount) {
+                rowCells.add(
+                    TableCell(
+                        child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(4.0),
+                                color: _getRandomColor(),
+                                border: Border.all(color: Colors.white)
+                            ),
+                            child: const Text(
+                              ' ',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white),
+                            )
+                        )
+                    )
+                );
+              } else {
+                rowCells.add(
+                    TableCell(
+                      child: Container(),
+                    )
+                );
+              }
+            }
+            return TableRow(children: rowCells);
+          }),
+        ),const SizedBox(height: 40),
+        Table(
+          children: List.generate(rows, (row) {
+            List<TableCell> rowCells = [];
+            for (int column = 0; column < columns; column++) {
+              int cellidx = (row * columns + column);
+              if (cellidx < cellCount) {
+                rowCells.add(
+                    TableCell(
+                        child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(4.0),
+                                color: _getRandomColor(),
+                                border: Border.all(color: Colors.white)
+                            ),
+                            child: const Text(
+                              ' ',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white),
+                            )
+                        )
+                    )
+                );
+              } else {
+                rowCells.add(
+                    TableCell(
+                      child: Container(),
+                    )
+                );
+              }
+            }
+            return TableRow(children: rowCells);
+          }),
+        ),const SizedBox(height: 40),
+        Table(
+          children: List.generate(rows, (row) {
+            List<TableCell> rowCells = [];
+            for (int column = 0; column < columns; column++) {
+              int cellidx = (row * columns + column);
+              if (cellidx < cellCount) {
+                rowCells.add(
+                    TableCell(
+                        child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(4.0),
+                                color: _getRandomColor(),
+                                border: Border.all(color: Colors.white)
+                            ),
+                            child: const Text(
+                              ' ',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white),
+                            )
+                        )
+                    )
+                );
+              } else {
+                rowCells.add(
+                    TableCell(
+                      child: Container(),
+                    )
+                );
+              }
+            }
+            return TableRow(children: rowCells);
+          }),
+        ),const SizedBox(height: 40),
+        Table(
+          children: List.generate(rows, (row) {
+            List<TableCell> rowCells = [];
+            for (int column = 0; column < columns; column++) {
+              int cellidx = (row * columns + column);
+              if (cellidx < cellCount) {
+                rowCells.add(
+                    TableCell(
+                        child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(4.0),
+                                color: _getRandomColor(),
+                                border: Border.all(color: Colors.white)
+                            ),
+                            child: const Text(
+                              ' ',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white),
+                            )
+                        )
+                    )
+                );
+              } else {
+                rowCells.add(
+                    TableCell(
+                      child: Container(),
+                    )
+                );
+              }
+            }
+            return TableRow(children: rowCells);
+          }),
         ),
-      ),
-    );
+      ],
+    ));
+  }
+  Color _getRandomColor() {
+    Random rnd = Random();
+    if (rnd.nextBool()) {
+      return Colors.green;
+    } else {
+      return Colors.red;
+    }
   }
 }
